0edbf3728f1c5c65941a97d582b6448c47120b7f
Noman Arshad <nomanarshad94@yahoo.com>
2020-02-12 14:28:04 +0500

    update frequency query

--- hacker_rank/interview_preparation/dictionaries_and_hashmap/frequency_queries.py
+++ hacker_rank/interview_preparation/dictionaries_and_hashmap/frequency_queries.py
@@ -2,26 +2,42 @@
 
 import os
 from collections import Counter
+from collections import defaultdict
 
 
 # Complete the freqQuery function below.
 def freqQuery(queries):
-    l = []
     output = []
+    l = defaultdict(int)
     for i in queries:
         if i[0] == 1:
-            l.append(i[1])
-        elif i[0] == 2:
-            try:
-                l.remove(i[1])
-            except ValueError:
-                pass
+            l[i[1]] += 1
+        elif i[0] == 2 and l[i[1]] > 0:
+            l[i[1]] -= 1
         elif i[0] == 3:
-            freq1 = Counter(l)
-            if i[1] in freq1.values():
+            if i[1] in l.values():
                 output.append(1)
             else:
                 output.append(0)
+    return output
+
+
+def freqQuery1(queries):
+    d = defaultdict(int)
+    output = []
+    f = defaultdict(set)
+
+    for (c, v) in queries:
+        if c == 3:
+            output.append(int(len(f[v]) > 0))
+            continue
+        f[d[v]].discard(v)
+        if c == 1:
+            d[v] += 1
+        elif d[v] > 0:
+            d[v] -= 1
+        f[d[v]].add(v)
+
     return output
 
 
